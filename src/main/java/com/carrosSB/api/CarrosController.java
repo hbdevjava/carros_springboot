889142c008a284ca295b6a31fd94360d7d803fd5
Hebert Brito <hbdev1211@gmail.com>
2022-06-09 20:21:23 -0300

    security database get, post, put, delete

--- src/main/java/com/carrosSB/api/CarrosController.java
+++ src/main/java/com/carrosSB/api/CarrosController.java
@@ -6,8 +6,12 @@ package com.carrosSB.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,8 @@ import com.carrosSB.domain.CarroService;
 
 @RestController
 @RequestMapping("/api/v1/carros")
+//ex:http://localhost:8080/api/v1/carros/23 
+//ex: localhost:8080/api/v1/carros/tipo/luxo 
 public class CarrosController {
 	@Autowired 
 	private CarroService service;
@@ -34,4 +40,35 @@ public class CarrosController {
 	public Iterable<Carro> getcarrosBytipo(@PathVariable ("tipo") String tipo){
 		return service.getCarroByTipo(tipo);
 	}
+	
+	
+	//@RequestBody: pega o objeto completo, ele pega o Json do carro e converte 
+	//para um objeto desde que eles tenham os mesmos atributos;
+	@PostMapping
+	public String post(@RequestBody Carro carro) {
+		Carro c = service.insert(carro);
+		
+		
+		return "Carro salvo com sucesso: " + c.getId();
+	}
+	
+	@PutMapping("/{id}")
+	public String put(@PathVariable ("id") Long id, @RequestBody Carro carro ){
+		Carro c = service.update(carro);
+		
+		return "Carro Atualizado com sucesso: " + c.getId();
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable ("id") Long id) {
+		service.delete(id);
+		
+		return "Carro Deletado com sucesso";
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/carrosSB/domain/CarroService.java
+++ src/main/java/com/carrosSB/domain/CarroService.java
@@ -6,63 +6,55 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-@Service 
+import org.springframework.util.Assert;
+
+@Service
 public class CarroService {
-	
+
 	@Autowired
 	private CarroRepository rep;
-	
-	public Iterable<Carro> getCarros(){
+
+	public Iterable<Carro> getCarros() {
 		return rep.findAll();
 	}
-	
+
 	public Iterable<Carro> getCarroByTipo(String tipo) {
-		
+
 		return rep.findByTipo(tipo);
 	}
-	
-	public Optional<Carro> getcarroById(Long id){
+
+	public Optional<Carro> getcarroById(Long id) {
 		return rep.findById(id);
 	}
+
+	public Carro insert(Carro carro) {
+		return rep.save(carro);
+
+	}
 	
-	
-	public List<Carro> getCarrosFake(){
-		List<Carro> carros = new ArrayList<>();
+	public Carro update(Carro carro) {
 		
-		carros.add(new Carro (1L, "Fusca"));
-		carros.add(new Carro (2L, "Brasilia"));
-		carros.add(new Carro (3L, "Chevete"));
-		
-		
-		
-		return carros;
+		return rep.save(carro);
 	}
 
-
-
-
+	public void delete(Long id) {
+		Optional<Carro> carro = getcarroById(id);
+		if(carro.isPresent()) {
+			rep.deleteById(id);
+		}
+		
+	}
 	
 	
-	
-	
-	
+	public List<Carro> getCarrosFake() {
+		List<Carro> carros = new ArrayList<>();
 
+		carros.add(new Carro(1L, "Fusca"));
+		carros.add(new Carro(2L, "Brasilia"));
+		carros.add(new Carro(3L, "Chevete"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		return carros;
+	}
 
 
 
