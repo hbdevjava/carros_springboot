fce29ef8f89a021cacc35b7d1effbda844c1a8ff
Hebert Brito <hbdev1211@gmail.com>
2022-06-09 10:09:54 -0300

    security database class 19

---
+++ src/main/java/com/carrosSB/api/IndexController.java
@@ -0,0 +1,41 @@
+package com.carrosSB.api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+	@GetMapping
+	public String hello() {
+	     return "API dos Carros";
+	}
+	//para estudos
+	/*@PostMapping("/login")//@RequestParam: le os parametro da query atraves da ?
+	public String login(@RequestParam("login") String login,@RequestParam("senha") String senha) {
+		return " login "  + login  + " senha " + senha;
+	}*/
+	
+	@GetMapping("/login/{login}/{senha}")//@PathVariable: le os parametro da query atraves da /
+	public String loginn(@PathVariable("login") String login,@PathVariable("senha") String senha) {
+		return " login "  + login  + " senha " + senha;
+	}
+	
+	@GetMapping("/carro/id/{id}")//http://localhost:8080/carro/id/1...
+	public String getCarroById(@PathVariable("id") Long id) {
+	     return "Carro by id " + id;
+	}
+	
+	@GetMapping("/carro/tipo/{tipo}")//ex:http://localhost:8080/carro/tipo/1...
+	public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+		return "lista by tipo " + tipo;
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/carrosSB/CarrosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrosSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
